225da4d4023712855ab46bf6e18190ef27f6f19f
Alana Chigbrow <alana.chigbrow@gmail.com>
2019-06-13 10:37:34 -0600

    Added database service.

--- app/src/main/java/edu/cnm/deepdive/atthemovies/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/MainActivity.java
@@ -2,8 +2,6 @@ package edu.cnm.deepdive.atthemovies;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProviders;
-import edu.cnm.deepdive.atthemovies.viewmodel.MoviesViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +11,4 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
   }
 
-  @Override
-  protected void onStop() {
-    super.onStop();
-    MoviesViewModel viewModel = ViewModelProviders.of(this).get(MoviesViewModel.class);
-    viewModel.saveData(this);
-  }
 }
